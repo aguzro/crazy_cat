4cf2dd6a71f4835a0b799a0a832b586478e370d2
Allison Guzmán <allison@laboratoria.cl>
2017-02-16 01:36:29 -0300

    Se añade evento click para link de menu, para mostrar sección correspondiente y ocultar las otras

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
+	$(".button-collapse").sideNav();
+
 	$( "#btn-first" ).click(function() {
 		$( "header" ).addClass( "desaparecer" );
 		$( "#register" ).removeClass( "desaparecer" ).addClass( "aparecer" );
@@ -8,5 +10,11 @@ $( document ).ready(function() {
 		$( "header" ).addClass( "desaparecer" );
 		$( "#register" ).removeClass( "aparecer" ).addClass( "desaparecer" );
 		$( "#catplay" ).removeClass( "desaparecer" ).addClass( "aparecer" );
+	});
+	$( "#link-record" ).click(function() {
+		$( "header" ).addClass( "desaparecer" );
+		$( "#register" ).removeClass( "aparecer" ).addClass( "desaparecer" );
+		$( "#catplay" ).removeClass( "aparecer" ).addClass( "desaparecer" );
+		$( "#record" ).removeClass( "desaparecer" ).addClass( "aparecer" );
 	});
 });
